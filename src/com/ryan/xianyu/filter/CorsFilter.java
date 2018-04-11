8e86a1a8fd1560f22366c85f91c0259c0f2cd9c2
TongJG <1135847806@qq.com>
2018-04-11 15:47:52 +0800

    panpanmemeda

--- src/com/ryan/xianyu/filter/CorsFilter.java
+++ src/com/ryan/xianyu/filter/CorsFilter.java
@@ -10,7 +10,9 @@ public class CorsFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
-        httpServletResponse.addHeader("Access-Control-Allow-Origin", "*");
+//        httpServletResponse.addHeader("Access-Control-Allow-Origin", "*");
+        httpServletResponse.addHeader("Access-Control-Allow-Origin", "localhost:8081");
+        httpServletResponse.addHeader("Access-Control-Allow-Credentials", "true");
         filterChain.doFilter(servletRequest, httpServletResponse);
     }
 
